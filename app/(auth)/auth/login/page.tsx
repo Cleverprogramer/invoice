a98f45de3a77e2520a0c22a8034c1920814933e0
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-04 23:48:16 +0300

    refactor: update UI of login component and improve user session handling

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,7 +1,59 @@
 import React from "react";
 
-const LoginPage = () => {
-  return <div>hellow from login</div>;
+import { redirect } from "next/navigation";
+
+import { auth, signIn } from "@/utils/auth";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SubmitButton from "../_components/SubmitButton";
+
+const LoginPage = async () => {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen ">
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login in to your acount
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              action={async (formData: FormData) => {
+                "use server";
+                await signIn("nodemailer", formData);
+              }}
+              className="flex flex-col space-y-4">
+              <div className="flex flex-col gap-y-2">
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="hello@hello.com"
+                />
+              </div>
+              <SubmitButton />
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
