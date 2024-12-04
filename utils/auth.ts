2a42b404df1e2c0b4384d0d2dffb833559bf7d8d
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-04 23:41:43 +0300

    feat: update NextAuth.js with Node Miller and Drizzle ORM adapter

---
+++ utils/auth.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+import NextAuth from "next-auth";
+import Nodemailer from "next-auth/providers/nodemailer";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
