4f9d61b8a016d7104690ee255625a7f99d19043f
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-13 21:57:18 +0300

    feat: add utility function to validate invoice id

---
+++ utils/isValidUUID.ts
@@ -0,0 +1,5 @@
+export function isValidUUID(str: string): boolean {
+  const regex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+  return regex.test(str);
+}
