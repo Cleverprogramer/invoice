7e7e0f76dda1e5866463e519cc12e54d6a2565d2
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-14 23:43:02 +0300

    feat: render hero and navbar components on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Hero } from "@/components/ui/Her";
+import { Navbar } from "@/components/ui/Navbar";
+
 export default function Home() {
-  return <h1>hellow from next js</h1>;
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
