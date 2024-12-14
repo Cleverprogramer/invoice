734114fdd81dddce2e6dfada34b3dbc9bceb436b
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-14 22:23:00 +0300

    refactor: update and render analytics on dashboard page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -3,12 +3,18 @@ import React from "react";
 import { useAuth as UseAuth } from "@/hooks/useAuth";
 
 import DashboardBlocks from "./_components/DashboardBlocks";
+import InvoiceGraph from "./_components/InvoiceGraph";
+import { RecentInvoices } from "./_components/RecentInvoices";
 
 const DashboardPage = async () => {
   const session = await UseAuth();
   return (
     <>
       <DashboardBlocks />
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-3">
+        <InvoiceGraph />
+        <RecentInvoices />
+      </div>
     </>
   );
 };
