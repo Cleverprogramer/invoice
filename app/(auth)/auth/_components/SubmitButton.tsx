5c36eba4d33c61427022abe88997eb050586499f
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-14 10:30:42 +0300

    refactor: update submit button to support dynamic variants via props

--- app/(auth)/auth/_components/SubmitButton.tsx
+++ app/(auth)/auth/_components/SubmitButton.tsx
@@ -5,17 +5,30 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 
-const SubmitButton = ({ label }: { label: string }) => {
+interface ISubmitButton {
+  label: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+}
+
+const SubmitButton = ({ label, variant }: ISubmitButton) => {
   const { pending } = useFormStatus();
   return (
     <>
       {pending ? (
-        <Button disabled className="w-full">
+        <Button variant={variant} disabled className="w-full">
           <Loader2 className="ml-2 size-2 animate-spin" />
           Please wait...
         </Button>
       ) : (
-        <Button type="submit" className="w-full">
+        <Button variant={variant} type="submit" className="w-full">
           {label}
         </Button>
       )}
