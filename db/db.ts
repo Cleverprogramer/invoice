d6fc718398ef34e4c12252fdd04d8ccebd1e6be2
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-12-14 10:41:53 +0300

    refactor: update database and pass dynamic data for tables

--- db/db.ts
+++ db/db.ts
@@ -2,7 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 import postgres from "postgres";
 
+import * as invoicesSchema from "./schema/invoice";
+import * as usersSchema from "./schema/auth";
+
 const connectionString = process.env.DATABASE_URL!;
 const pool = postgres(connectionString, { max: 1 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, {
+  schema: { ...usersSchema, ...invoicesSchema },
+});
